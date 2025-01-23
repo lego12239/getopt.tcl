bd3432eb8270ab8c300c8a1d8513f1f064bdbe7c
Oleg Nemanov <lego12239@yandex.ru>
2025-01-23 13:23:49 +0300

    add an util template

---
+++ util_tmpl.tcl
@@ -0,0 +1,39 @@
+#!/usr/bin/tclsh
+
+lappend auto_path ~/work/libs/tcl/
+package require getopt
+
+
+proc show_help {} {
+	puts "[file tail $::argv0] \[OPTIONS] ARG"
+	puts " OPTIONS:"
+}
+
+proc getopt_cb {_priv oname oval} {
+	upvar 2 $_priv priv
+
+	switch $oname {
+	-h -
+	--help {
+		show_help
+		exit 0
+	}
+	-v -
+	--version {
+		puts "Version 0.1"
+		exit 0
+	}
+	default {
+		puts "Unknown option: $oname"
+		exit 1
+	}
+	}
+
+	return 0
+}
+
+#################################################################
+# MAIN
+#################################################################
+set priv {}
+getopt::parse {getopt_cb priv} argv
